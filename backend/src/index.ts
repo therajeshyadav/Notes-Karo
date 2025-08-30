e9297f01d71ccb887d4236629d71333e4db921ba
therajeshyadav <140382326+therajeshyadav@users.noreply.github.com>
2025-08-30 23:53:57 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,8 @@ import noteRoutes from "../routes/noteRoutes";
 const app = express();
 connectDB();
 
-app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3002", "http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3002", "http://localhost:5173",
+                "https://note-karo-nqt331xis-rajesh-s-projects-187b6c1a.vercel.app"], credentials: true }));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
