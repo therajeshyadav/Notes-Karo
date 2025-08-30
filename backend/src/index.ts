317ce3ec7f1ad6a61c15f7210b6d942d27a8d047
therajeshyadav <140382326+therajeshyadav@users.noreply.github.com>
2025-08-31 00:14:51 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,9 +8,23 @@ import noteRoutes from "../routes/noteRoutes";
 
 const app = express();
 connectDB();
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3002",
+  "http://localhost:5173",
+  process.env.VITE_API_URL
+];
 
-app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3002", "http://localhost:5173",
-                "https://note-karo-nqt331xis-rajesh-s-projects-187b6c1a.vercel.app"], credentials: true }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
